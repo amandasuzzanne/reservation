5069f2901615dd2c822a54fff047decc111a1f4e
amandasuzzanne <amandasuzanne12@gmail.com>
2023-05-03 00:11:42 +0300

    Update page title

--- mnglogin.php
+++ mnglogin.php
@@ -29,7 +29,7 @@ session_start(); //session to manage
 <!DOCTYPE html>
 <html>
 <head>
-<title>Manager Login</title>
+<title>ADMIN LOGIN</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
