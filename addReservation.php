7e9a51d5d915de2a6fa5b7bdefbf70a506584e3d
amandasuzzanne <amandasuzanne12@gmail.com>
2023-05-03 00:10:17 +0300

    Update page title

--- addReservation.php
+++ addReservation.php
@@ -39,7 +39,7 @@ include('configuration.php'); //using database connection file here
 <!DOCTYPE html>
 <html>
 <head>
-<title>Add User</title>
+<title>RESERVATION</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <div class="container">
--- sendMessage.php
+++ sendMessage.php
@@ -29,7 +29,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Contact Us</title>
+<title>CONTACT US</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <div class="container">
